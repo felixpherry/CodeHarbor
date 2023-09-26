ac44fa272f6981426f71ed6af69ab6e76d217735
Felix Pherry <123felixpherry@gmail.com>
2023-09-26 23:35:11 +0700

    Fix styling

--- components/shared/ProfileMenu.tsx
+++ components/shared/ProfileMenu.tsx
@@ -39,14 +39,16 @@ import Image from 'next/image';
 export const ProfileMenu = ({ session }: { session: SessionInterface }) => {
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Image
-          src={session.user?.image || '/avatar-fallback.svg'}
-          height={40}
-          width={40}
-          alt='profile image'
-          className=' object-cover rounded-full w-[40px] h-[40px]'
-        />
+      <DropdownMenuTrigger className='inline-flex items-center justify-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground px-4 py-2 relative h-8 w-8 rounded-full'>
+        <span className='relative flex shrink-0 overflow-hidden rounded-full h-10 w-10'>
+          <Image
+            src={session.user?.image || '/avatar-fallback.svg'}
+            height={40}
+            width={40}
+            alt='profile image'
+            className='aspect-square h-full w-full'
+          />
+        </span>
       </DropdownMenuTrigger>
       <DropdownMenuContent className='w-56 mr-8'>
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
