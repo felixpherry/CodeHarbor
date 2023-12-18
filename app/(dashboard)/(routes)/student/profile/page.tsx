2db9ee137047c2b15e490f3aae0a4a1de8a09410
I00794.FELIX <felix.pherry@kawanlamacorp.com>
2023-12-18 16:57:23 +0700

    Fix the styling of student profile page

--- app/(dashboard)/(routes)/student/profile/page.tsx
+++ app/(dashboard)/(routes)/student/profile/page.tsx
@@ -5,7 +5,7 @@ import { db } from '@/lib/db';
 import { getCurrentUser } from '@/lib/session';
 import { convertToTitleCase } from '@/lib/utils';
 import { SessionInterface } from '@/types';
-import { CalendarDays, Mail, MapPin, Phone } from 'lucide-react';
+import { CalendarDays, Mail, MapPin, Pencil, Phone } from 'lucide-react';
 import moment from 'moment';
 import Image from 'next/image';
 import { notFound, redirect } from 'next/navigation';
@@ -81,50 +81,69 @@ const Page = async () => {
   ];
 
   return (
-    <div className='flex flex-col p-8 gap-10'>
-      <div className='flex justify-between items-start'>
-        <div className='flex items-center gap-5'>
-          <Image
-            src={session.user.image}
-            alt='Profile Photo'
-            width={128}
-            height={128}
-            className='rounded-full h-32 w-32'
-          />
-          <div className='flex flex-col gap-5'>
-            <div className='flex flex-col gap-1'>
-              <h3 className='font-bold text-2xl'>{accountDetail?.name}</h3>
-              <p className='text-base'>
-                {accountDetail?.username ? `@${accountDetail.username}` : '-'}
-              </p>
-            </div>
-            <div className='flex items-center gap-2'>
-              <CalendarDays className='h-4 w-4' />
-              Joined {moment(accountDetail?.createdAt).format('MMMM YYYY')}
+    <div className='flex flex-col gap-5'>
+      <div className='flex flex-col gap-6 bg-white p-8 rounded-md shadow'>
+        <div className='flex flex-col md:flex-row md:justify-between items-start gap-5'>
+          <div className='flex flex-col md:flex-row md:items-center gap-5'>
+            <Image
+              src={session.user.image}
+              alt='Profile Photo'
+              width={128}
+              height={128}
+              className='rounded-full h-32 w-32'
+            />
+            <div className='flex flex-col gap-5'>
+              <div className='flex flex-col gap-1'>
+                <h3 className='font-bold text-2xl'>{accountDetail?.name}</h3>
+                <p className='text-base'>
+                  {accountDetail?.username ? `@${accountDetail.username}` : '-'}
+                </p>
+              </div>
+              <div className='flex items-center gap-2'>
+                <CalendarDays className='h-4 w-4' />
+                Joined {moment(accountDetail?.createdAt).format('MMMM YYYY')}
+              </div>
             </div>
           </div>
-        </div>
-        <Button variant='primary-blue' className='shadow-sm'>
-          Edit Profile
-        </Button>
-      </div>
+          <Button
+            variant='edit'
+            className='shadow-sm hidden md:flex'
+            size='sm'>
+            <Pencil className='h-4 w-4' />
+            Edit
+          </Button>
 
-      <div className='flex items-center gap-6 flex-wrap'>
-        {accountDetailMap.map(({ icon: Icon, label, value }) => (
-          <div key={label} className='flex items-center gap-1'>
-            <ActionTooltip label={label}>
-              <Icon className='w-4 h-4' />
-            </ActionTooltip>
-            {value}
-          </div>
-        ))}
+          <Button
+            className='block md:hidden w-full rounded-full font-sans'
+            variant='primary-blue-outline'
+            size='sm'>
+            Edit Profile
+          </Button>
+        </div>
+
+        <div className='flex items-center gap-x-6 gap-y-3 flex-wrap'>
+          {accountDetailMap.map(({ icon: Icon, label, value }) => (
+            <div
+              key={label}
+              className='flex items-center gap-1'>
+              <ActionTooltip label={label}>
+                <Icon className='w-4 h-4' />
+              </ActionTooltip>
+              {value}
+            </div>
+          ))}
+        </div>
       </div>
 
       <div className='flex flex-col gap-5 rounded-md shadow bg-white p-5'>
-        <h3 className='text-xl font-bold text-primary'>Student Information</h3>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+        <h3 className='text-xl font-bold text-primary pb-3 border-b'>
+          Student Information
+        </h3>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-2'>
           {studentInformationMap.map(({ label, value }) => (
-            <div key={label} className='flex flex-col'>
+            <div
+              key={label}
+              className='flex flex-col'>
               <h5 className='text-primary font-semibold'>{label}</h5>
               <p className='text-muted-foreground'>{value}</p>
             </div>
